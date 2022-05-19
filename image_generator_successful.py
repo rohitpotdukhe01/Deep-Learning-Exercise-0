4fe9ab2e2a03d554b97b8c15aae9b4034eaf3008
Chitra23Ahuja <105649388+Chitra23Ahuja@users.noreply.github.com>
2022-05-19 23:24:31 +0200

    Update image_generator_successful.py

--- image_generator_successful.py
+++ image_generator_successful.py
@@ -1,28 +1,18 @@
-import random
-import sys
-import os
-import os.path
 import json
-
-import plotly.tools
-import scipy.misc
-import numpy as np
+import collections
 import matplotlib.pyplot as plt
+import random
+import numpy as np
 import skimage.transform
-
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
-from pathlib import Path
 
-
-# In this exercise task you will implement an image generator. Generator objects in python are defined as having a
-# next function. This next function returns the next generated object. In our case it returns the input of a neural
-# network each time it gets called. This input consists of a batch of images and its corresponding labels.
+# In this exercise task you will implement an image generator. Generator objects in python are defined as having a next function.
+# This next function returns the next generated object. In our case it returns the input of a neural network each time it gets called.
+# This input consists of a batch of images and its corresponding labels.
 class ImageGenerator:
-    file_path = label_path = batch_size = image_size = rotation = mirroring = shuffle = 0
     batch_epoch = current_ep = 0
-    label_data = file_data = 0
 
     def __init__(self, file_path, label_path, batch_size, image_size, rotation=False, mirroring=False, shuffle=False):
         # Define all members of your generator class object as global members here.
@@ -38,108 +28,102 @@ class ImageGenerator:
         self.label_path = label_path
         self.batch_size = batch_size
         self.image_size = image_size
+
         self.rotation = rotation
         self.mirroring = mirroring
         self.shuffle = shuffle
+
         self.class_dict = {0: 'airplane', 1: 'automobile', 2: 'bird', 3: 'cat', 4: 'deer', 5: 'dog', 6: 'frog',
                            7: 'horse', 8: 'ship', 9: 'truck'}
-
-        self.label_data = json.load(open(self.label_path))
-
-        if shuffle:
-            self.file_data = []
-            for (dirpath, dirnames, filenames) in os.walk(self.file_path):
-                self.file_data.extend(filenames)
-                print(filenames)
-                break
-            random.shuffle(self.file_data)
+        # TODO: implement constructor
 
     def next(self):
         # This function creates a batch of images and corresponding labels and returns them.
         # In this context a "batch" of images just means a bunch, say 10 images that are forwarded at once.
         # Note that your amount of total data might not be divisible without remainder with the batch_size.
         # Think about how to handle such cases
-
-        pth, dirs, files = next(os.walk(self.file_path))
-        file_count = len(files)
-
-        if self.batch_epoch >= file_count - 1:
+        # TODO: implement next method
+        with open(self.label_path) as f:
+            data1 = json.load(f)
+        count = len(data1)
+        ims = []
+        lbl = []
+        if (self.batch_epoch >= count - 1):
             self.current_ep += 1
             self.batch_epoch = 0
+        h = self.batch_epoch
 
-        ims = []
-        labs = []
-        idx = self.batch_epoch
-        for s in range(self.batch_epoch, self.batch_epoch + self.batch_size):
-            if s % file_count == 0:
-                if self.shuffle:
-                    random.shuffle(self.file_data)
-                if idx >= file_count:
-                    idx = 0
-            if self.shuffle:
-                next_path = os.path.join(self.file_path, self.file_data[idx])
-                img_file = np.load(next_path)
-                img_file = skimage.transform.resize(img_file, self.image_size)
-                if self.mirroring or self.rotation:
-                    img_file = self.augment(img_file)
-                ims.append(img_file)
-                labs.append(self.label_data[self.file_data[idx].split('.')[0]])
-            else:
-                file_name = str(idx) + ".npy"
-                next_path = os.path.join(self.file_path, file_name)
-                img_file = np.load(next_path)
-                img_file = skimage.transform.resize(img_file, self.image_size)
-                if self.mirroring or self.rotation:
-                    img_file = self.augment(img_file)
-                ims.append(img_file)
-                labs.append(self.label_data[str(idx)])
-            idx += 1
+        def split_dict(d, n, h):
+            keys = list(d.keys())
+            for i in range(0, 1):
+                yield {k: d[k] for k in keys[h:h + n]}
 
-        images = np.array(ims)
-        labels = np.array(labs)
-        self.batch_epoch += self.batch_size
-        print(images,labels)
+            # data =sorted(data1.items(), key=lambda x: x[1])
+
+        if self.shuffle:
+            data = collections.OrderedDict(sorted(data1.items()))
+        else:
+            data = collections.OrderedDict(data1.items())
+
+        for item in split_dict(data, self.batch_size, h):
+            images_data = list(item.keys())
+            labels_data = list(item.values())
+            #length = len(images_data)
+            # Iterating the index
+            # same as 'for i in range(len(list))'
+
+            for x in images_data:
+                img = np.load(self.file_path + x + '.npy')
+                img = skimage.transform.resize(img, self.image_size)
+                img = self.augment(img)
+                ims.append(img)
+                index = images_data.index(x)
+                lbl.append(self.class_name(labels_data[index]))
+            h += 1
+            images = np.array(ims)
+            labels = np.array(lbl)
+            self.batch_epoch += self.batch_size
         return images, labels
 
     def augment(self, img):
         # this function takes a single image as an input and performs a random transformation
         # (mirroring and/or rotation) on it and outputs the transformed image
-
-        if self.mirroring and random.randint(0, 1) == 1:
+        # TODO: implement augmentation function
+        if self.mirroring and random.randint(0, 1) == 0:
             img = np.flip(img, 1)
-
         if self.rotation and random.randint(0, 1) == 1:
-            img = np.rot90(img, random.randint(1, 3))
+            img = np.rot90(img, random.randint(0, 3))
 
         return img
+
+    def class_name(self, x):
+        # This function returns the class name for a specific input
+        # TODO: implement class name function
+        return self.class_dict[x]
 
     def current_epoch(self):
         # return the current epoch number
         return self.current_ep
 
-    def class_name(self, x):
-        # This function returns the class name for a specific input
-        return self.class_dict[x]
-
     def show(self):
         # In order to verify that the generator creates batches as required, this functions calls next to get a
         # batch of images and labels and visualizes it.
+        # TODO: implement show method
         batch = self.next()
-        print(batch)
-        rows = int(np.ceil(len(batch[0]) / 3))
+        rows = int(np.ceil(len(batch[0] / 3)))
         cols = 3
-
         titles = []
-        for tit in range(0, len(batch[1])):
-            titles.append(self.class_name(batch[1][tit]))
+        for y in batch[1]:
+            titles.append(y)
         fig = make_subplots(rows=rows, cols=cols, subplot_titles=titles)
 
         i = j = 1
-        for image_idx in range(0, len(batch[0])):
+        for x in batch[0]:
             if j > cols:
                 j = 1
                 i += 1
-            fig.add_trace(go.Image(z=batch[0][image_idx] * 255), i, j)
+
+            fig.add_trace(go.Image(z=x * 255), i, j)
             fig.update_xaxes(visible=False, showticklabels=False)
             fig.update_yaxes(visible=False, showticklabels=False)
             j += 1
